47612373b61cbc4061baef0330cb060d7efa46c5
annametsapelto <anna.metsapelto@gmail.com>
2020-07-03 14:43:01 +0300

    Add basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './Components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+          <h1>Order yourself a waffle!</h1>
+        </Layout>
       </div>
     );
   }
