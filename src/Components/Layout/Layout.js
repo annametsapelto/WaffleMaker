7d5c196a1b47fa2a2219697798c6ebdab095f9a3
annametsapelto <anna.metsapelto@gmail.com>
2020-07-17 15:44:49 +0300

    Add toolbar and adjust margins

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
-import classes from './Layout.css';
+import './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
-      <div>
-          Toolbar,
-          Sidedrawer,
-          Backdrop
-      </div>
-      <main className={classes.Content}>
+      <Toolbar></Toolbar>
+      <main className="Content">
           {props.children}
       </main>
     </Aux>
